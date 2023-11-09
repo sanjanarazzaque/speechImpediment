5d9d51995752d77573151d2c1ec1fa493a306841
Sanjana Razzaque <sanjanarazzaque@wireless-10-146-118-83.public.utexas.edu>
2023-11-08 21:12:42 -0600

    Communit1 start

--- frontend/App.js
+++ frontend/App.js
@@ -4,13 +4,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
 import LoginScreen from './components/Login'; 
+import Community1 from './components/Community1';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Community1">
+        <Stack.Screen name = "Community1" component={Community1} options={{headerShown: false}} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
---
+++ frontend/components/Community1.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Community1 = () => {
+    const [selectedTab, setSelectedTab] = useState('Discover');
+
+    const renderContent = () => {
+        if (selectedTab === 'Discover') {
+            return (
+                <View style={styles.contentContainer}>
+                    {/* Content for Discover tab */}
+                    <Text>Discover Tab Content</Text>
+                   
+                </View>
+            );
+        } else if (selectedTab === 'YourGroups') {
+            return (
+                <View style={styles.contentContainer}>
+                    {/* Content for Your Groups tab */}
+                    <Text>Your Groups Tab Content</Text>
+                </View>
+            );
+        }
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.tabContainer}>
+                <TouchableOpacity onPress={() => setSelectedTab('Discover')} style={selectedTab === 'Discover' ? styles.selectedTab : styles.tab}>
+                    <Text style={styles.tabText}>Discover</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setSelectedTab('YourGroups')} style={selectedTab === 'YourGroups' ? styles.selectedTab : styles.tab}>
+                    <Text style={styles.tabText}>Your Groups</Text>
+                </TouchableOpacity>
+            </View>
+            {renderContent()}
+        </SafeAreaView>
+    );
+};
+
+export default Community1;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    tabContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: 'blue',
+    },
+    tab: {
+        paddingVertical: 10,
+    },
+    selectedTab: {
+        paddingVertical: 10,
+        borderBottomWidth: 2,
+        borderBottomColor: 'blue',
+    },
+    tabText: {
+        fontSize: 18,
+        color: 'black',
+    },
+    contentContainer: {
+        flex: 1,
+    },
+});
